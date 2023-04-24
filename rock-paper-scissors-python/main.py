1d256e9f166b4f9b0e3def6e975c79b2a214b5ec
Nathálya Ricci <102884724+nathalya-ricci@users.noreply.github.com>
2023-04-24 14:31:52 -0300

    format code

--- rock-paper-scissors-python/main.py
+++ rock-paper-scissors-python/main.py
@@ -1,64 +1,69 @@
 import random
 
-OPTIONS = ['rock', 'paper', 'scissors']
+OPTIONS = ["rock", "paper", "scissors"]
 
 player_score = 0
 computer_score = 0
 
 
 def get_computer_choice():
+    """Get a random computer choice"""
+
     computer_choice = random.choice(OPTIONS)
     return computer_choice
 
 
 def get_player_choice():
-    # Get the player input and prompt for a choice while it's not in the options list
+    """
+    Get the player input and prompt for a choice
+    while it's not in the options list
+    """
 
     while True:
         player_choice = input(
-            'Please, choose between rock, paper, or scissors: ').lower()
+            "Please, choose between rock, paper, or scissors: "
+        ).lower()
 
         if player_choice in OPTIONS:
             return player_choice
         else:
-            print('Invalid choice!')
+            print("Invalid choice!")
 
 
 def play_round():
-    # Play a round and update the scoreboard
+    """Play a round and update the scoreboard"""
 
     global player_score, computer_score
 
     player_selection = get_player_choice()
     computer_selection = get_computer_choice()
 
-    print(
-        f'You chose {player_selection} and the computer chose {computer_selection}')
+    print(f"You chose {player_selection} and the computer chose {computer_selection}")
 
     round_result = check_round_winner(player_selection, computer_selection)
 
-    if round_result == 'You win!':
+    if round_result == "You win!":
         player_score += 1
-    elif round_result == 'Computer wins!':
+    elif round_result == "Computer wins!":
         computer_score += 1
 
-    print(f'Your score: {player_score}')
-    print(f'Computer score: {computer_score}')
+    print(f"Your score: {player_score}")
+    print(f"Computer score: {computer_score}")
 
 
 def check_round_winner(player_selection, computer_selection):
-    # Check the round results
+    """Check the round results"""
 
     results = {
-        'rockscissors': 'You win!',
-        'paperrock': 'You win!',
-        'scissorspaper': 'You win!',
-        'rockpaper': 'Computer wins!',
-        'paperscissors': 'Computer wins!',
-        'scissorsrock': 'Computer wins!',
-        'rockrock': 'It\'s a tie!',
-        'paperpaper': 'It\'s a tie!',
-        'scissorsscissors': 'It\'s a tie!',
+        "rockscissors": "You win!",
+        "paperrock": "You win!",
+        "scissorspaper": "You win!",
+        "rockpaper": "Computer wins!",
+        "paperscissors": "Computer wins!",
+        "scissorsrock": "Computer wins!",
+        "rockrock": "It's a tie!",
+        "paperpaper": "It's a tie!",
+        "scissorsscissors": "It's a tie!",
     }
 
     result_key = player_selection + computer_selection
@@ -66,17 +71,17 @@ def check_round_winner(player_selection, computer_selection):
 
 
 def play_game():
-    # Executes the game 5 times
+    """Executes the game 5 times"""
 
-    for i in range(5):
+    for _ in range(5):
         play_round()
 
     if player_score > computer_score:
-        print('Congrats! You won the game!')
+        print("Congrats! You won the game!")
     elif computer_score > player_score:
-        print('Sorry, the computer won the game :(')
+        print("Sorry, the computer won the game :(")
     else:
-        print('The game ended in a tie!')
+        print("The game ended in a tie!")
 
 
 play_game()
